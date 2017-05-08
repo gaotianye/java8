3bd1df38df7c1871b44ccec68df3846698f55891
gaotianye <15156506755@163.com>
2017-05-08 16:21:38 +0800

    update：修改注释

--- Java8/src/cn/gaotianye/lambda/Lambda3.java
+++ Java8/src/cn/gaotianye/lambda/Lambda3.java
@@ -10,9 +10,9 @@ public class Lambda3 {
 		 * 方法引用语法格式有3种：
 			(1)objectName::instanceMethod
 			(2)ClassName::staticMethod
-				等同于把lambda表达式的参数直接当成instanceMethod|staticMethod的参数来调用。
-				比如System.out::println等同于x->System.out.println(x)；
-					Math::max等同于(x, y)->Math.max(x,y)。
+				1和2等同于把lambda表达式的参数直接当成instanceMethod|staticMethod的参数来调用。
+				比如System.out::println等同于x->System.out.println(x);
+					Math::max等同于(x, y)->Math.max(x,y)
 			(3)ClassName::instanceMethod
 				等同于把lambda表达式的第一个参数当成instanceMethod的目标对象，
 				其他剩余参数当成该方法的参数。
