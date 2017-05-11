3edca51a6d462bb78655e80f134c73c7dc84491a
gaotianye <15156506755@163.com>
2017-05-11 16:36:22 +0800

    update：ArrayList与LinkedList的对比

--- Java8/src/cn/gaotianye/actionWithWrong/Test4.java
+++ Java8/src/cn/gaotianye/actionWithWrong/Test4.java
@@ -9,4 +9,12 @@ public class Test4 {
 	 * 		程序员们经常使用public作为类字段的修饰符，可以很简单的通过引用得到值，
 	 * 		但这是一个坏的设计，按照经验，分配给成员变量的访问级别应该尽可能的低。
 	 */
+	/**
+	 * 3、ArrayList与LinkedList的对比
+	 * 		当程序员们不知道ArrayList与LinkedList的区别时，他们经常使用ArrayList，
+	 * 		因为它看起来比较熟悉。然而，它们之前有巨大的性能差别。
+	 * 		简而言之，如果有大量的增加删除操作并且没有很多的随机访问元素的操作，
+	 * 		应该首先LinkedList。
+	 * 		如果你刚接触它们，请查看 ArrayList vs. LinkedList来获得更多关于它们性能的信息。
+	 */
 }
