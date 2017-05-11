a5e01bd75e4f6ea781cf21e615f406154bcff7fe
gaotianye <15156506755@163.com>
2017-05-11 15:14:33 +0800

    add:Opitional 解决空指针问题

---
+++ Java8/src/cn/gaotianye/nulls/OptionalTest.java
@@ -0,0 +1,30 @@
+package cn.gaotianye.nulls;
+
+import java.util.Optional;
+
+public class OptionalTest {
+	/**
+	 * 	Full Name is set? false
+		Full Name: [none]
+		Hey Stranger!
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/**
+		 * 如果Optional类的实例为非空值的话，isPresent()返回true，否从返回false。
+		 * 为了防止Optional为空值，orElseGet()方法通过回调函数来产生一个默认值。
+		 * map()函数对当前Optional的值进行转化，然后返回一个新的Optional实例。
+		 * orElse()方法和orElseGet()方法类似，但是orElse接受一个默认值而不是一个回调函数。
+		 */
+		Optional< String > fullName = Optional.ofNullable("liushu");
+		System.out.println( "Full Name is set? " + fullName.isPresent() );        
+		System.out.println( "Full Name: " + fullName.orElseGet( () -> "[none]" ) ); 
+		System.out.println( fullName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+		System.out.println("===================");
+		Optional< String > firstName = Optional.of("gaotianye");
+		System.out.println( "First Name is set? " + firstName.isPresent() );        
+		System.out.println( "First Name: " + firstName.orElseGet( () -> "[none]" ) ); 
+		System.out.println( firstName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+		System.out.println();
+	}
+}
