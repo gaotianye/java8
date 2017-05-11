7dcc70be3e74411244febcb73f1acaeac6db1d56
gaotianye <15156506755@163.com>
2017-05-11 16:35:21 +0800

    udpate：访问级别（分配给成员变量的访问级别应该尽可能的低）

--- Java8/src/cn/gaotianye/actionWithWrong/Test4.java
+++ Java8/src/cn/gaotianye/actionWithWrong/Test4.java
@@ -4,5 +4,9 @@ public class Test4 {
 	 * 1、 HashMap和HashTable区别？
 			HashTable是同步的，HashMap是异步的，所以HashMap比较常用
 	 */
-	
+	/**
+	 * 2、访问级别
+	 * 		程序员们经常使用public作为类字段的修饰符，可以很简单的通过引用得到值，
+	 * 		但这是一个坏的设计，按照经验，分配给成员变量的访问级别应该尽可能的低。
+	 */
 }
