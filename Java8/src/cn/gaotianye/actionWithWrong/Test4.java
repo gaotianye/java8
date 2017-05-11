a40c8aa57facbe26a22eba9d26f83d9e96611b32
gaotianye <15156506755@163.com>
2017-05-11 16:34:29 +0800

    update：HashMap和HashTable区别

--- Java8/src/cn/gaotianye/actionWithWrong/Test4.java
+++ Java8/src/cn/gaotianye/actionWithWrong/Test4.java
@@ -1,8 +1,8 @@
 package cn.gaotianye.actionWithWrong;
-/**
- * HashMap和HashTable区别？
- * HashTable是同步的，HashMap是异步的，所以HashMap比较常用。
- * @author gaotianye
- *
- */
-public class Test4 {}
+public class Test4 {
+	/**
+	 * 1、 HashMap和HashTable区别？
+			HashTable是同步的，HashMap是异步的，所以HashMap比较常用
+	 */
+	
+}
